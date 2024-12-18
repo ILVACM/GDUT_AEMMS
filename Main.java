1c0d60986def015b9eacfe9f1bcb4d8da83ac04c
DELL-Inspiron15R-Win10-hbk <3347620766@qq.com>
2024-12-18 21:02:53 +0800

    dataclass bts update

--- Main.java
+++ Main.java
@@ -1,22 +1,32 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+// import java.util.Arrays;
+// import java.util.Collections;
+// import java.util.Comparator;
 
-public class Main {
-    public static void main(String[] args) {
-        Integer[] array = {5, 3, 8, 4, 2};
+import src.DataClass.*;
+
+public class Main 
+{
+    // public static void main(String[] args) {
+    //     Integer[] array = {5, 3, 8, 4, 2};
         
-        // 对于Integer数组使用Collections.reverseOrder()进行降序排序
-        Arrays.sort(array, Collections.reverseOrder());
-        System.out.println(Arrays.toString(array));
+    //     // 对于Integer数组使用Collections.reverseOrder()进行降序排序
+    //     Arrays.sort(array, Collections.reverseOrder());
+    //     System.out.println(Arrays.toString(array));
         
-        // 或者定义自己的Comparator来实现降序排序
-        Arrays.sort(array, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2 - o1; // 降序
-            }
-        });
-        System.out.println(Arrays.toString(array));
+    //     // 或者定义自己的Comparator来实现降序排序
+    //     Arrays.sort(array, new Comparator<Integer>() {
+    //         @Override
+    //         public int compare(Integer o1, Integer o2) {
+    //             return o2 - o1; // 降序
+    //         }
+    //     });
+    //     System.out.println(Arrays.toString(array));
+    // }
+
+    public static void main(String[] args) 
+    {
+        node n1 = new node(1, (short)1, (short)1);
+        BTS bts = new BTS();
+        bts.add(n1);
     }
 }
--- src/DataClass/BTS.java
+++ src/DataClass/BTS.java
@@ -1,20 +1,97 @@
 package src.DataClass;
 
 import java.util.*;
+
 import src.DataClass.node;
 
 public class BTS
 {
     /*
-    //
+     * 基础数据结构方法实现
+     * 增删查改
      */
 
     // BST 基底数据储存数组
-    public ArrayList<node> DataStore = new ArrayList<>();
+    private ArrayList<node> DataStore;
 
     // BST 检索数组
-    public ArrayList<Integer> DataIndex = new ArrayList<Integer>();
+    private ArrayList<Integer> DataIndex;
 
     // BST 辅助标记数组
-    public  ArrayList<Boolean> mark = new ArrayList<Boolean>();
+    private ArrayList<Boolean> mark;
+
+    // BST 特征记录成员变量?????????
+    public int amount;
+    public int deepth;
+    public boolean order;
+
+    // BST 辅助变量
+    private int temp;
+
+    // BST 构造函数
+    public BTS()
+    {
+        /*
+         * 构造函数，初始化数据结构
+         * .........
+         */
+
+        // 核心数据数组创建
+        this.DataStore = new ArrayList<>();
+        this.DataIndex = new ArrayList<Integer>();
+        this.mark = new ArrayList<Boolean>();
+
+        // 关键特征变量赋值
+        amount = 0;
+        deepth = 0;
+        order = true;
+
+    }
+
+    // BST 深度更新函数
+    public int updateDeepth()
+    {
+        /*
+         * 更新深度
+         * .........
+         */
+
+        temp = amount;
+
+        do 
+        {
+            temp /= 2;
+            deepth++;
+        } 
+        while ( temp > 0 );
+
+        return 0;
+    }
+
+    // BST 实例添加新的数据节点（两种添加方式，通过重载实现）
+    public int add(node New)
+    {
+        DataStore.add(New);
+
+        amount++;
+
+        updateDeepth();
+
+        order = false;
+
+        return 0;
+    }
+
+    public int add(int PID, short PNo, short port)
+    {
+        DataStore.add( new node(PID, PNo, port));
+
+        amount++;
+
+        updateDeepth();
+
+        order = false;
+        
+        return 0;
+    }
 }
