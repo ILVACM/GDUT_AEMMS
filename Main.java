aa929c526ae989f0d95196cf0903e111f2c8e491
DELL-Inspiron15R-Win10-hbk <3347620766@qq.com>
2024-12-18 15:58:04 +0800

    dataclass define

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
-public class test {
+public class Main {
     public static void main(String[] args) {
         Integer[] array = {5, 3, 8, 4, 2};
         
